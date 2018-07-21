4711dee2b73dee87c172c6b2b4f9382955b56d27
Kevin Maurer <kevinmaurer2011@gmail.com>
2018-07-21 17:48:25 -0500

    updated **kwargs

--- python-masterclass/music-browser/jukebox.py
+++ python-masterclass/music-browser/jukebox.py
@@ -7,6 +7,20 @@ except ImportError:  # python 2
 
 conn = sqlite3.connect('music.db', )
 
+
+class Scrollbox(tkinter.Listbox):
+
+    def __init__(self, window, **kwargs):
+        super().__init__(window, **kwargs)
+
+        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=self.yview)
+
+    def grid(self, row, column, sticky='nsw', rowspan=1, columnspan=1, **kwargs):
+        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan, **kwargs)
+        self.scrollbar.grid(row=row, column=column, sticky='nse', rowspan=rowspan)
+        self['yscrollcommand'] = self.scrollbar.set
+
+
 main_window = tkinter.Tk()
 main_window.title('Music DB Browser')
 main_window.geometry('1024x768')
@@ -27,29 +41,21 @@ tkinter.Label(main_window, text='Albums').grid(row=0, column=1)
 tkinter.Label(main_window, text='Songs').grid(row=0, column=2)
 
 # ==== Artists Listbox =====
-artist_list = tkinter.Listbox(main_window)
+artist_list = Scrollbox(main_window)
 artist_list.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artist_list.config(border=2, relief='sunken')
-
-artist_scroll = tkinter.Scrollbar(main_window, orient=tkinter.VERTICAL, command=artist_list.yview)
-artist_scroll.grid(row=1, column=0, sticky='nse', rowspan=2)
-artist_list['yscrollcommand'] = artist_scroll.set
 
 # ==== Albums Listbox =====
 album_lv = tkinter.Variable(main_window)
 album_lv.set(('Choose an artist',))
-album_list = tkinter.Listbox(main_window, listvariable=album_lv)
+album_list = Scrollbox(main_window, listvariable=album_lv)
 album_list.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 album_list.config(border=2, relief='sunken')
-
-album_scroll = tkinter.Scrollbar(main_window, orient=tkinter.VERTICAL, command=album_list.yview)
-album_scroll.grid(row=1, column=1, sticky='nse', rowspan=2)
-album_list['yscrollcommand'] = album_scroll.set
 
 # ==== Songs Listbox =====
 song_lv = tkinter.Variable(main_window)
 song_lv.set(('Choose an album',))
-song_list = tkinter.Listbox(main_window, listvariable=song_lv)
+song_list = Scrollbox(main_window, listvariable=song_lv)
 song_list.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 song_list.config(border=2, relief='sunken')
 
