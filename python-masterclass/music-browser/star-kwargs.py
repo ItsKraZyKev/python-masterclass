204d7f9a31ed777dacc7dbb2229b6415ad4c0d2a
Kevin Maurer <kevinmaurer2011@gmail.com>
2018-07-21 17:11:50 -0500

    updated **kwargs

--- python-masterclass/music-browser/star-kwargs.py
+++ python-masterclass/music-browser/star-kwargs.py
@@ -1,8 +1,17 @@
 def print_backwards(*args, **kwargs):
-    print(kwargs)
-    for word in args[::-1]:
-        print(word[::-1], end=' ', **kwargs)
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', ' ')
+    for word in args[:0:-1]:
+        print(word[::-1], end=sep_character, **kwargs)
+    print(args[0][::-1], end=end_character, **kwargs)  # print first word seperatly
+    # print (end=end_character)
 
 
 with open('backwards.txt', 'w') as backwards:
-    print_backwards('hello', 'planet', 'earth', 'take', 'me', 'to', 'your', 'leader', file=backwards)
+    print_backwards('hello', 'planet', 'earth', 'take', 'me', 'to', 'your', 'leader', end='\n')
+    print('Another string')
+
+print()
+print('hello', 'planet', 'earth', 'take', 'me', 'to', 'your', 'leader', end='', sep='\n**\n')
+print_backwards('hello', 'planet', 'earth', 'take', 'me', 'to', 'your', 'leader', end='', sep='\n**\n')
+print('=' * 10)
