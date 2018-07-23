b60ef281db7508c7528c3df5b6c57fe2e7868d0c
Kevin Maurer <kevinmaurer2011@gmail.com>
2018-07-23 15:17:17 -0500

    Final code

--- python-masterclass/generator-examples/sizes.py
+++ python-masterclass/generator-examples/sizes.py
@@ -10,22 +10,23 @@ def my_range(n: int):
         start += 1
 
 
-_ = input('line 12')
-# big_range = range(5)
-big_range = my_range(5)
-_ = input('line 15'
-          '')
+big_range = range(5)
+# big_range = my_range(5)
+
+
 print(f'big_range is {sys.getsizeof(big_range)} bytes')
 
 # create a list containing all the values in big_range
 big_list = []
 
-_ = input('line 22')
 for val in big_range:
-    _ = input('line 24 - inside loop')
     big_list.append(val)
 
 print(f'big_list is {sys.getsizeof(big_list)} bytes')
 
 print(big_range)
 print(big_list)
+
+print('looping again... or not')
+for i in big_range:
+    print(f'i is {i}')
