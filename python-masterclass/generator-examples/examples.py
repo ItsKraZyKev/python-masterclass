2d6222ea0c20b09631a155dc4e2fc998e785fa20
Kevin Maurer <kevinmaurer2011@gmail.com>
2018-07-25 13:12:16 -0500

    Final code

---
+++ python-masterclass/generator-examples/examples.py
@@ -0,0 +1,14 @@
+text = "What have the romans ever done for us"
+
+capitals = [char.upper() for char in text]
+
+print(capitals)
+
+words = [word.upper() for word in text.split(' ')]
+print(words)
+
+lc_words = text.split(' ')
+print(lc_words)
+
+lc_words = [word for word in text.split(' ')]
+print(lc_words)
